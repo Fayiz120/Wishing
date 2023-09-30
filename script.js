d0388b2c79b4bf7f26a8cf3b471857f35b9d4e46
fayiz120 <mfaisep@gmail.com>
2023-09-30 14:58:45 +0530

    added script tag

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var currentURL = window.location.href;
 var url = new URL(currentURL);
+console.log(url)
 var searchParams = url.searchParams;
 var extractedValue = searchParams.get('?=');
+console.log(extractedValue);
 document.getElementById('extractedValue').textContent = 'Extracted Value: ' + extractedValue;
