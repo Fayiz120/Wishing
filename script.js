83e0ffda3c56ec1fd69132166415d039b9683c43
fayiz120 <mfaisep@gmail.com>
2023-09-30 19:06:39 +0530

    Added dynamic link generation

--- script.js
+++ script.js
@@ -9,7 +9,22 @@ function getUrlParameter(name) {
 var nameParam = getUrlParameter('name');
 
 if (nameParam) {
-    document.getElementById('value').innerHTML='<p>Name: ' + nameParam + '</p>';
+    document.getElementById('value').innerHTML=  nameParam + ' send You a wish';
 } else {
-    document.write('<p>No name parameter found.</p>');
+    document.getElementById('value').innerHTML=  'Send You a wish';
 }
+function assignLink(){
+
+    var name = document.getElementById('nameInput').value;
+    if (name.trim() !== '') {
+        var link = 'https://fayiz120.github.io/Wishing/?name=' + encodeURIComponent(name);
+        var whatsappLink  = "whatsapp://send?text=" + link ;
+        console.log("changed")
+        document.getElementById('what').href = whatsappLink;
+    } else {
+        document.getElementById('generatedLink').innerHTML = 'Please enter a name.';
+    }
+}
+// document.getElementById('generateLinkButton').addEventListener('click', function() {
+   
+// });
