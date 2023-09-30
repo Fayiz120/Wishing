ae9aff57be7d16762f9c375fa17646c857d97c87
fayiz120 <mfaisep@gmail.com>
2023-09-30 17:22:26 +0530

    Changed Script

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-var currentURL = window.location.href;
-var url = new URL(currentURL);
-console.log(url)
-var searchParams = url.searchParams;
-var extractedValue = searchParams.get('?=');
-console.log(extractedValue);
-document.getElementById('extractedValue').textContent = 'Extracted Value: ' + extractedValue;
+function getUrlParameter(name) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+    var results = regex.exec(window.location.href);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+var nameParam = getUrlParameter('name');
+
+if (nameParam) {
+    document.getElementById('value').innerHTML='<p>Name: ' + nameParam + '</p>';
+} else {
+    document.write('<p>No name parameter found.</p>');
+}
